42b526e602af7527b5ff3948bc5846b5bfb3075b
陈嘉进 <chenjiajin0725@163.com>
2025-05-13 23:19:23 +0800

    default init

---
+++ src/main/java/com/chenjiajin/Application.java
@@ -0,0 +1,18 @@
+package com.chenjiajin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author com.chenjiajin
+ * @date 2022/2/16
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
